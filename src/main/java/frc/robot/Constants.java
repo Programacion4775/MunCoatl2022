ccd8c6cf5c9f5ec20fc6d85771d623d165aaee0b
AztechProgramacion <programacion4775@gmail.com>
2022-03-15 17:48:51 -0600

    ID

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -7,4 +7,9 @@ public final static int TraccionBackLeft = 1;
 public final static int TraccionBackRight = 3; 
 public final static int TraccionFrontLeft = 7; 
 public final static int TraccionFrontRight = 13; 
+//PARKOUR//
+public final static int TelescopicoRightBack = 30; 
+public final static int TelescopicoLeftBack = 31; 
+public final static int TelescopicoRightFront = 32; 
+public final static int TelescopicoLeftFront = 33; 
 }

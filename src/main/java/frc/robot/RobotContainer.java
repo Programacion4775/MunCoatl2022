46518f5ecda3554fc161c5ad6a51a7cd67e9a27d
AztechProgramacion <programacion4775@gmail.com>
2022-03-21 13:49:48 -0600

    autonomowaaaaaa

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -11,7 +11,6 @@ import frc.robot.commands.CargoCommands.UpBallsCom;
 import frc.robot.commands.Parkour.TelescopicoCom;
 import frc.robot.commands.ShooterCommands.ShooterComControl;
 import frc.robot.commands.ShooterCommands.ShooterComVels;
-import frc.robot.commands.TraccionCommands.TraccionAutoHorizontal;
 import frc.robot.commands.TraccionCommands.TraccionAutoVertical;
 import frc.robot.commands.TraccionCommands.TraccionCom;
 import frc.robot.subsystems.Traccion;
@@ -29,47 +28,50 @@ import edu.wpi.first.wpilibj2.command.WaitCommand;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 
 /**
- * This class is where the bulk of the robot should be declared. Since Command-based is a
- * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
- * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
+ * This class is where the bulk of the robot should be declared. Since
+ * Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in
+ * the {@link Robot}
+ * periodic methods (other than the scheduler calls). Instead, the structure of
+ * the robot (including
  * subsystems, commands, and button mappings) should be declared here.
  */
 
 public class RobotContainer {
 
-//COMMANDS AND SUBSYSTEMS DECLARATION//
-//DECLARACION DE SUBSISTEMAS Y COMANDOS//
-  
-  //Traccion//
+  // COMMANDS AND SUBSYSTEMS DECLARATION//
+  // DECLARACION DE SUBSISTEMAS Y COMANDOS//
+
+  // Traccion//
   private final Traccion r_Traccion = new Traccion();
   private final TraccionCom r_TraccionCom = new TraccionCom(r_Traccion);
-  //Intake//
+  // Intake//
   private final IntakeFront r_IntakeFront = new IntakeFront();
   private final IntakeBack r_IntakeBack = new IntakeBack();
-  //UpBalls//
+  // UpBalls//
   private final UpBalls r_UpBalls = new UpBalls();
   private final UpBallsCom r_UpBallsCom = new UpBallsCom(r_UpBalls);
-  //Shooter//
+  // Shooter//
   private final Shooter r_Shooter = new Shooter();
   private final ShooterComControl r_ShooterComControl = new ShooterComControl(r_Shooter);
-  //Telescopico//
+  // Telescopico//
   private final Telescopico r_Telescopico = new Telescopico();
-  private final TelescopicoCom r_TelescopicoCom = new TelescopicoCom(r_Telescopico); 
-  //Limelight//
+  private final TelescopicoCom r_TelescopicoCom = new TelescopicoCom(r_Telescopico);
+  // Limelight//
   public static final Limelight r_Limelight = new Limelight();
 
-//DECLARACION DE CONTROLES//
-  public static final XboxController Control0 = new XboxController(0);   //Control 0//
-  public static final XboxController Control1 = new XboxController(1);  //Control 1//
-  public static final XboxController Control2 = new XboxController(2); //Control 2//
+  // DECLARACION DE CONTROLES//
+  public static final XboxController Control0 = new XboxController(0); // Control 0//
+  public static final XboxController Control1 = new XboxController(1); // Control 1//
+  public static final XboxController Control2 = new XboxController(2); // Control 2//
 
-//DECLARACION DE BOTONES//
-  //Control 0//
+  // DECLARACION DE BOTONES//
+  // Control 0//
   public JoystickButton ButtonA_0 = new JoystickButton(Control0, 1);
   public JoystickButton ButtonB_0 = new JoystickButton(Control0, 2);
   public JoystickButton ButtonX_0 = new JoystickButton(Control0, 3);
   public JoystickButton ButtonY_0 = new JoystickButton(Control0, 4);
-  //Control 1//
+  // Control 1//
   public JoystickButton ButtonA_1 = new JoystickButton(Control1, 1);
   public JoystickButton ButtonB_1 = new JoystickButton(Control1, 2);
   public JoystickButton ButtonX_1 = new JoystickButton(Control1, 3);
@@ -78,7 +80,7 @@ public class RobotContainer {
   public JoystickButton BumperR_1 = new JoystickButton(Control1, 6);
   public JoystickButton Button7_1 = new JoystickButton(Control1, 7);
   public JoystickButton Button8_1 = new JoystickButton(Control1, 8);
-  //Control2//
+  // Control2//
   public JoystickButton DriverS1 = new JoystickButton(Control2, 1);
   public JoystickButton DriverS8 = new JoystickButton(Control2, 8);
   public JoystickButton DriverS9 = new JoystickButton(Control2, 9);
@@ -88,38 +90,37 @@ public class RobotContainer {
 
     configureButtonBindings();
 
-//BUTTONS AND COMMANS ASIGNATION//
-//ASIGNACION DE BOTONES A COMANDOS// 
+    // BUTTONS AND COMMANS ASIGNATION//
+    // ASIGNACION DE BOTONES A COMANDOS//
 
-  //Control 1//    
-  ButtonA_1.whenHeld(new IntakeComFront(r_IntakeFront, -1));
-  ButtonB_1.whenHeld(new IntakeComBack(r_IntakeBack, 1));
-  ButtonX_1.whenHeld(new IntakeComFront(r_IntakeFront, 1));
-  ButtonY_1.whenHeld(new IntakeComBack(r_IntakeBack, -1));
-  BumperL_1.whenHeld(new ShooterComVels(r_Shooter, .21));      //Shooter velocity 1 - Velocidad 1 shooter//
-  BumperR_1.whenHeld(new ShooterComVels(r_Shooter, .21));     //Shooter velocity 2 - Velocidad 2 shooter//         
+    // Control 1//
+    ButtonA_1.whenHeld(new IntakeComFront(r_IntakeFront, -1));
+    ButtonB_1.whenHeld(new IntakeComBack(r_IntakeBack, 1));
+    ButtonX_1.whenHeld(new IntakeComFront(r_IntakeFront, 1));
+    ButtonY_1.whenHeld(new IntakeComBack(r_IntakeBack, -1));
+    BumperL_1.whenHeld(new ShooterComVels(r_Shooter, .21)); // Shooter velocity 1 - Velocidad 1 shooter//
+    BumperR_1.whenHeld(new ShooterComVels(r_Shooter, .21)); // Shooter velocity 2 - Velocidad 2 shooter//
 
-  //Control 2//
+    // Control 2//
 
-
-
-
-
-//COMANDOS DE DEFAULT//
-//DEFEAULT COMMANDS//
-  r_Traccion.setDefaultCommand(r_TraccionCom);
-  r_UpBalls.setDefaultCommand(r_UpBallsCom);
-  r_Shooter.setDefaultCommand(r_ShooterComControl);
-  r_Telescopico.setDefaultCommand(r_TelescopicoCom);
+    // COMANDOS DE DEFAULT//
+    // DEFEAULT COMMANDS//
+    r_Traccion.setDefaultCommand(r_TraccionCom);
+    r_UpBalls.setDefaultCommand(r_UpBallsCom);
+    r_Shooter.setDefaultCommand(r_ShooterComControl);
+    r_Telescopico.setDefaultCommand(r_TelescopicoCom);
   }
 
   /**
-   * Use this method to define your button->command mappings. Buttons can be created by
+   * Use this method to define your button->command mappings. Buttons can be
+   * created by
    * instantiating a {@link GenericHID} or one of its subclasses ({@link
-   * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a {@link
+   * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing
+   * it to a {@link
    * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
    */
-  private void configureButtonBindings() {}
+  private void configureButtonBindings() {
+  }
 
   /**
    * Use this to pass the autonomous command to the main {@link Robot} class.
@@ -127,32 +128,44 @@ public class RobotContainer {
    * @return the command to run in autonomous
    */
   public Command getAutonomousCommand() {
-    return
-     new ParallelCommandGroup(
-      new ShooterComVels(r_Shooter, .5),
-      new SequentialCommandGroup(
-        new WaitCommand(2),
-        new TraccionAutoVertical(r_Traccion, -2048, -2048),
-        new IntakeComBack(r_IntakeBack, 1),
-        new UpBallsAutoCom(r_UpBalls, 1),
-        new WaitCommand(2),
-        new TraccionAutoHorizontal(r_Traccion, 2048, -2048),
-        new TraccionAutoVertical(r_Traccion, -1000, -1000),
-        new IntakeComBack(r_IntakeBack, 1),
-        new UpBallsAutoCom(r_UpBalls, 1),
-        new WaitCommand(2),
-        new TraccionAutoVertical(r_Traccion, -900, 900)
-      )
-      )
- /*return new ParallelCommandGroup( 
-    new ShooterComVels(r_Shooter, .21),
-    new SequentialCommandGroup(new WaitCommand(3)),
-      new IntakeComFront(r_IntakeFront, -1),
-      new UpBallsAutoCom(r_UpBalls, 1),
-      new SequentialCommandGroup(
-        new WaitCommand(5),
-        new ParallelDeadlineGroup(new TraccionAutoVertical(r_Traccion, 50, 50),
-        new WaitCommand(1)
-        )));
-  }*/
+    return Fwd();
+
   }
+
+  public Command Red (){
+      return
+        new SequentialCommandGroup(
+          new WaitCommand(2),
+          new TraccionAutoVertical(r_Traccion, -128442, -128442),
+          new ParallelCommandGroup(
+            new ShooterComVels(r_Shooter, 1),       
+            new SequentialCommandGroup(
+              new WaitCommand(2),   
+              new ParallelDeadlineGroup(
+                new WaitCommand(7), 
+                new IntakeComBack(r_IntakeBack, 1), 
+                new UpBallsAutoCom(r_UpBalls, 1))),
+          new WaitCommand(2),
+          new TraccionAutoVertical(r_Traccion, 13975, -13975),//2969
+          new WaitCommand(2),
+          new TraccionAutoVertical(r_Traccion, 138077, 138077),
+          new ParallelDeadlineGroup(
+            new WaitCommand(7), 
+            new IntakeComBack(r_IntakeBack, 1), 
+            new UpBallsAutoCom(r_UpBalls, 1)),
+          new TraccionAutoVertical(r_Traccion, -56355, 56355),//900
+          new TraccionAutoVertical(r_Traccion, -247447, -247447),
+          new TraccionAutoVertical(r_Traccion, 247447, 247447),
+          new ParallelCommandGroup(
+            new ShooterComVels(r_Shooter, 1),       
+            new SequentialCommandGroup(
+              new WaitCommand(2),   
+              new ParallelDeadlineGroup(
+                new WaitCommand(7), 
+                new UpBallsAutoCom(r_UpBalls, 1))))));
+    }
+
+  public Command Fwd() {
+    return new TraccionAutoVertical(r_Traccion, 45812, 45812);
+  }
+}
